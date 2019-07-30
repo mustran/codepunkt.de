adf11658a3d9de82c66deb1f36242a043bbcc77d
Christoph Werner <christoph@codepunkt.de>
2019-07-30 12:43:51 +0000

    fix: older browser tweet embed style

--- src/style/embedStyle.js
+++ src/style/embedStyle.js
@@ -2,7 +2,8 @@ import { css } from '@emotion/core'
 
 export const twitterStyle = css`
   /* horizontally center twitter widget */
-  twitter-widget {
+  twitter-widget,
+  iframe.twitter-tweet {
     margin-left: auto;
     margin-right: auto;
   }
