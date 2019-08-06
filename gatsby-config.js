416b443d0e8cec7a19f102f9ef9445218c69b3c7
Christoph Werner <christoph@codepunkt.de>
2019-08-06 16:26:22 +0000

    fix: adjust max-width for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 1200,
+              maxWidth: 728,
               linkImagesToOriginal: false,
             },
           },
