d8ab692ccbc39f48656565b4337ea3e44a7d611b
Christoph Werner <christoph@codepunkt.de>
2019-11-18 15:34:36 +0000

    fix: font-size on mobile

--- src/style/typography.js
+++ src/style/typography.js
@@ -27,7 +27,9 @@ const typography = new Typography({
   includeNormalize: false,
   overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
     '@media only screen and (max-width: 667px)': {
-      html: { font: `100%/1.72 'Open sans',serif;` },
+      html: {
+        font: `112.5%/1.72 'Open Sans',-apple-system,'BlinkMacSystemFont','Segoe UI','Roboto','Oxygen-Sans','Ubuntu','Cantarell','Helvetica Neue',sans-serif;`,
+      },
       code: { fontSize: '16px' },
     },
     code: {
